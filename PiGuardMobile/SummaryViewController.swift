03ca1367d4e381cbe0bae9205c0195f466bb4558
freedom <stefano.vettor@mac.com>
2016-04-04 20:39:17 +0200

    adding gitignore

---
+++ PiGuardMobile/SummaryViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PiGuardMobile
+//
+//  Created by Stefano Vettor on 02/04/16.
+//  Copyright © 2016 Stefano Vettor. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
